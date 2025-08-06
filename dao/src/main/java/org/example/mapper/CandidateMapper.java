ba2435067c4b907d3cd1defa4f7f1261afd32020
Mercer-Su <374962954@qq.com>
2025-08-06 14:46:08 +0800

    chore:新增加mapper

---
+++ dao/src/main/java/org/example/mapper/CandidateMapper.java
@@ -0,0 +1,16 @@
+package org.example.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.example.pojo.Candidate;
+
+/**
+ * <p>
+ * 应聘者表 Mapper 接口
+ * </p>
+ *
+ * @author bilibili觉哥学Java
+ * @since 2024-10-04
+ */
+public interface CandidateMapper extends BaseMapper<Candidate> {
+
+}
