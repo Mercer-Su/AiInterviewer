9ae81eeaece6ac58a0203201f9c5ec8b3b87dcb3
Mercer-Su <374962954@qq.com>
2025-08-07 10:36:34 +0800

    chore:数据持久层

---
+++ pojo/src/main/java/org/example/pojo/bo/JobBO.java
@@ -0,0 +1,21 @@
+package org.example.pojo.bo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobBO {
+
+    private String id;
+    private String jobName;
+    private String jobDesc;
+    private Integer status;
+    private String interviewerId;
+    private String prompt;
+
+}
