99cc5580b46d9ec50e23c88b5a76e34eb4b50f27
Mercer-Su <374962954@qq.com>
2025-08-09 10:28:40 +0800

    feat:文件配置

---
+++ api/src/main/java/org/example/controller/FileController.java
@@ -0,0 +1,78 @@
+package org.example.controller;
+
+import jakarta.annotation.Resource;
+import org.apache.commons.lang3.StringUtils;
+import org.example.base.MinIOConfig;
+import org.example.base.MinIOUtils;
+import org.example.grace.result.GraceJSONResult;
+import org.example.grace.result.ResponseStatusEnum;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+/**
+ * @ClassName FileController
+ * @Version 1.0
+ * @Description FileController
+ **/
+@RestController
+@RequestMapping("file")
+public class FileController {
+
+    @Resource
+    private MinIOConfig minIOConfig;
+
+    @PostMapping("uploadInterviewerImage")
+    public GraceJSONResult uploadInterviewerImage(@RequestParam("file") MultipartFile file) throws Exception {
+
+        // 获得文件原始名称
+        String filename = file.getOriginalFilename();
+        if (StringUtils.isBlank(filename)) {
+            return GraceJSONResult.errorCustom(ResponseStatusEnum.FILE_UPLOAD_NULL_ERROR);
+        }
+
+        filename = "interviewer/" + dealWithoutFilename(filename);
+        String imageUrl = MinIOUtils.uploadFile(minIOConfig.getBucketName(),
+                                                filename,
+                                                file.getInputStream(),
+                                                true);
+
+        return GraceJSONResult.ok(imageUrl);
+    }
+
+    @PostMapping("uploadInterviewVideo")
+    public GraceJSONResult uploadInterviewVideo(@RequestParam("file") MultipartFile file) throws Exception {
+
+        // 获得文件原始名称
+        String filename = file.getOriginalFilename();
+        if (StringUtils.isBlank(filename)) {
+            return GraceJSONResult.errorCustom(ResponseStatusEnum.FILE_UPLOAD_NULL_ERROR);
+        }
+
+        filename = "interviewVideo/" + dealWithoutFilename(filename);
+        String videoUrl = MinIOUtils.uploadFile(minIOConfig.getBucketName(),
+                filename,
+                file.getInputStream(),
+                true);
+
+        return GraceJSONResult.ok(videoUrl);
+    }
+
+    private String dealWithFilename(String filename) {
+        String suffixName = filename.substring(filename.lastIndexOf("."));
+        String fName = filename.substring(0, filename.lastIndexOf("."));
+        String uuid = UUID.randomUUID().toString();
+        return fName + "-" + uuid + suffixName;
+    }
+
+    private String dealWithoutFilename(String filename) {
+        String suffixName = filename.substring(filename.lastIndexOf("."));
+        String uuid = UUID.randomUUID().toString();
+        return uuid + suffixName;
+    }
+
+}
